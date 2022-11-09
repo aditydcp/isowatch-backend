bcf114dda31ea389646e5162d7cf8950f8b28e75
Aditya Cahyo <aditydcp@gmail.com>
2022-11-09 16:22:48 +0700

    create register pasien endpoint

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 // require database connection 
 const dbConnect = require("./db/dbConnect");
 
+// require data models
+const Pasien = require("./db/pasienModel");
+
 // execute database connection 
 dbConnect();
 
@@ -17,5 +20,28 @@ app.get("/", (request, response, next) => {
   next();
 });
 
+// endpoints
+app.post("/register", (request, response) => {
+  // initialize new Pasien object with params from the req
+  const pasien = new Pasien({
+    idPasien: request.body.idPasien,
+    namaPasien: request.body.namaPasien,
+  });
+
+  // save the data
+  pasien.save()
+  .then((result) => {
+    response.status(201).send({
+      message: "Pasien registered successfully",
+      result,
+    });
+  })
+  .catch((error) => {
+    response.status(500).send({
+      message: "Error registering pasien",
+      error,
+    });
+  })
+});
 
 module.exports = app;

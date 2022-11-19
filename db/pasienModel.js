2fe3c5994dd5c826e3b43f84715ec06ca9af997e
Aditya Cahyo <aditydcp@gmail.com>
2022-11-19 19:07:40 +0700

    update fields

--- db/pasienModel.js
+++ db/pasienModel.js
@@ -10,6 +10,31 @@ const PasienSchema = new mongoose.Schema({
         required: [true, "Please provide a name!"],
         unique: false,
     },
+    tanggalLahir: {
+        type: Date,
+        required: [true, "Please provide birthdate!"],
+        unique: false,
+    },
+    alamat: {
+        type: String,
+        required: [true, "Please provide address!"],
+        unique: false,
+    },
+    gender: {
+        type: String,
+        required: [true, "Please provide gender!"],
+        unique: false,
+    },
+    keluhan: {
+        type: String,
+        required: false,
+        unique: false,
+    },
+    riwayatPenyakit: {
+        type: String,
+        required: false,
+        unique: false,
+    },
 })
 
 module.exports = mongoose.model("Pasien", PasienSchema);
